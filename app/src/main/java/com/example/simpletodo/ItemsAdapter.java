3d11ca7d28003ca16bde46b4de585a8e9b8505c7
huilin <zouhuilin5@gmail.com>
2021-08-18 03:29:44 -0400

    Initial commit

---
+++ app/src/main/java/com/example/simpletodo/ItemsAdapter.java
@@ -0,0 +1,73 @@
+package com.example.simpletodo;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder>{
+
+    public interface OnLongClickListener{
+        void onItemLongClicked(int position);
+    }
+
+    List<String> items;
+    OnLongClickListener longClickListener;
+
+    public ItemsAdapter(List<String> items, OnLongClickListener longClickListener){
+        this.items=items;
+        this.longClickListener=longClickListener;
+    }
+    @NonNull
+    @Override
+
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        View todoView;
+        todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1,parent, false);
+        return new ViewHolder(todoView);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position){
+        String item=items.get(position);
+        holder.bind(item);
+
+    }
+
+    @Override
+    public int getItemCount(){
+        return items.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView tvItem;
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+            tvItem=itemView.findViewById(android.R.id.text1);
+        }
+
+        public void bind(String item){
+
+            tvItem.setText(item);
+            tvItem.setOnLongClickListener(new View.OnLongClickListener(){
+                @Override
+                public  boolean onLongClick (View view){
+                    longClickListener.onItemLongClicked(getAdapterPosition());
+                    return true;
+                }
+
+            });
+        }
+
+
+    }
+
+}
+
